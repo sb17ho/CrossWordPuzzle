4ae429064b0c4e40077079af7d24c234ca14fbca
sb17ho <sb17ho@brocku.ca>
2020-02-28 13:36:48 -0500

    Tweaks

--- src/Puzzle/PuzzleSolver.kt
+++ src/Puzzle/PuzzleSolver.kt
@@ -2,25 +2,28 @@ package Puzzle
 
 import java.util.*
 
-class puzzleSolver{
+class puzzleSolver {
     val scanner: Scanner = Scanner(System.`in`);
 
     //User will be displayed "Found a Word?", if yes where, else loop
-    fun found(){
+    fun found() {
         var found: Boolean = false
+        var quite: String = ""
 
-        println("_________Game Started________\n\nFound a Word!")
-        while (!found){
-            if(scanner.next() == "yes"){
+        //TODO: See how to provide quite option
+        println("_________Game Started________\n\nWrite Quite Anytime to Exit the Game and see the answers\n\nFound a word?")
+        while (!found || quite == "quite") {
+            if (scanner.next() == "yes") {
                 where()
-//                break
-            }else{
+                println("Found a Word?")
+//                found = true
+            } else {
                 continue
             }
         }
     }
 
-    fun where(){
+    fun where() {
         println("In where")
     }
 }
